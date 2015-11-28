179464b4ed6f7e1bbef62d9795f2ddf99adb1ab7
zhuzhi <zhuzhi2013@gmail.com>
2015-11-28 10:58:39 +0800

    Initial Commit

---
+++ SpeedyTransfer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpeedyTransfer
+//
+//  Created by zhuzhi on 15/11/28.
+//  Copyright © 2015年 ZZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
