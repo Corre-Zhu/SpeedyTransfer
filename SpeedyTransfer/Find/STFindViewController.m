150e4574229cafe1f544914734bd69533ab206b3
zerozhu <zhuzhi2013@gmail.com>
2016-05-23 10:38:46 +0800

    discover

--- SpeedyTransfer/Find/STFindViewController.m
+++ SpeedyTransfer/Find/STFindViewController.m
@@ -8,7 +8,10 @@
 
 #import "STFindViewController.h"
 
-@interface STFindViewController ()
+@interface STFindViewController ()<UIWebViewDelegate>
+{
+	UIActivityIndicatorView *activityIndicatorView;
+}
 
 @property (nonatomic, strong) UIWebView *webView;
 
@@ -23,15 +26,32 @@
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"left_white"] style:UIBarButtonItemStylePlain target:self action:@selector(leftBarButtonItemClick)];
     
     _webView = [[UIWebView alloc] init];
+	_webView.backgroundColor = [UIColor whiteColor];
+	_webView.delegate = self;
     [self.view addSubview:_webView];
     [_webView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
-    
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]];
+	
+	NSString *urlString = [NSString stringWithFormat:@"http://www.3tkj.cn/jurl/j.php?id=%@", [[UIDevice currentDevice] openUDID]];
+	NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [_webView loadRequest:request];
+	
+	activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+	[activityIndicatorView startAnimating];
+	[self.view addSubview:activityIndicatorView];
+	[activityIndicatorView autoCenterInSuperview];
 }
 
 - (void)leftBarButtonItemClick {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+	[activityIndicatorView stopAnimating];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(nullable NSError *)error {
+	[activityIndicatorView stopAnimating];
 }
 
 @end
