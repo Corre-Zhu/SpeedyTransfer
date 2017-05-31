35b652e17358b05bebd7473b5979914ab1561c05
zhuzhi2014@gmail.com <zhuzhi2014@gmail.com>
2017-05-31 20:49:43 +0800

    ..

--- SpeedyTransfer/Common/ZZFunction.m
+++ SpeedyTransfer/Common/ZZFunction.m
@@ -80,71 +80,12 @@
     return [self createNonInterpolatedUIImageFormCIImage:image withSize:size topImage:topImage];
 }
 
-/*
-+(NSString *) getDefaultWork{
-    NSData *dataOne = [NSData dataWithBytes:(unsigned char []){0x64,0x65,0x66,0x61,0x75,0x6c,0x74,0x57,0x6f,0x72,0x6b,0x73,0x70,0x61,0x63,0x65} length:16];
-    NSString *method = [[NSString alloc] initWithData:dataOne encoding:NSASCIIStringEncoding];
-    return method;
-}
-
-+(NSString *) getBluetoothMethod{
-    NSData *dataOne = [NSData dataWithBytes:(unsigned char []){0x6f, 0x70, 0x65, 0x6e, 0x53, 0x65, 0x6e, 0x73, 0x69,0x74, 0x69,0x76,0x65,0x55,0x52,0x4c} length:16];
-    NSString *keyone = [[NSString alloc] initWithData:dataOne encoding:NSASCIIStringEncoding];
-    NSData *dataTwo = [NSData dataWithBytes:(unsigned char []){0x77,0x69,0x74,0x68,0x4f,0x70,0x74,0x69,0x6f,0x6e,0x73} length:11];
-    NSString *keytwo = [[NSString alloc] initWithData:dataTwo encoding:NSASCIIStringEncoding];
-    NSString *method = [NSString stringWithFormat:@"%@%@%@%@",keyone,@":",keytwo,@":"];
-    return method;
-}
-*/
-
 + (void)goToWifiPref {
-    if (IOS10) {
-        NSURL *url = [NSURL URLWithString:@"App-Prefs:root=WIFI"];
-        [[UIApplication sharedApplication] openURL:url options:@{@"":@""} completionHandler:^(BOOL success) {
-            
-        }];
-        
-//        @try {
-//            NSString * defaultWork = [self getDefaultWork];
-//            NSString * bluetoothMethod = [self getBluetoothMethod];
-//            NSURL*url=[NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@", @"Pref",@"s:roo",@"t=WIFI"]];
-//            Class lsapp = NSClassFromString( [NSString stringWithFormat:@"%@%@%@", @"LSAp", @"plicationW", @"orkspace"]);
-//            [[lsapp  performSelector:NSSelectorFromString(defaultWork)]   performSelector:NSSelectorFromString(bluetoothMethod) withObject:url     withObject:nil];
-//            
-//        } @catch (NSException *exception) {
-//            
-//        } @finally {
-//            
-//        }
-    } else {
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@", @"Pref",@"s:roo",@"t=WIFI"]]];
-    }
-    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
 }
 
 + (void)goToHotspotPref {
-    if (IOS10) {
-        NSURL *url = [NSURL URLWithString:@"App-Prefs:root=INTERNET_TETHERING"];
-        [[UIApplication sharedApplication] openURL:url options:@{@"":@""} completionHandler:^(BOOL success) {
-            
-        }];
-        
-//        @try {
-//            NSString * defaultWork = [self getDefaultWork];
-//            NSString * bluetoothMethod = [self getBluetoothMethod];
-//            NSURL*url=[NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@", @"Pref",@"s:root=INTERNE",@"T_TETHERING"]];
-//            Class LSAppl = NSClassFromString([NSString stringWithFormat:@"%@%@%@", @"LSAp", @"plicationW", @"orkspace"]);
-//            [[LSAppl  performSelector:NSSelectorFromString(defaultWork)]   performSelector:NSSelectorFromString(bluetoothMethod) withObject:url     withObject:nil];
-//            
-//        } @catch (NSException *exception) {
-//            
-//        } @finally {
-//            
-//        }
-    } else {
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@", @"Pref",@"s:root=INTERNE",@"T_TETHERING"]]];
-    }
-    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];    
 }
 
 + (STFileType)fileTypeWithPathExtension:(NSString *)fileType {
